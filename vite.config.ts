3c2d5972be008ec577edd690f2d3164e0e3b967b
otmjka <otmjka@gmail.com>
2025-10-27 07:10:56 +0100

    feat: add base to vite config for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wiam_tt/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
